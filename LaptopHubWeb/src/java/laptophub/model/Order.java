03e2010c12191a1da7e7bba7b5be76d3733fa6de
myduyen3103 <vtmyduyen3103@gmail.com>
2024-06-16 09:05:26 +0700

    config prj

---
+++ LaptopHubWeb/src/java/laptophub/model/Order.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laptophub.model;
+
+/**
+ *
+ * @author admin
+ */
+public class Order {
+    
+}
---
+++ LaptopHubWeb/src/java/laptophub/utils/DBConnection.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laptophub.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import laptophub.config.Configuration;
+
+/**
+ *
+ * @author admin
+ */
+public class DBConnection implements Configuration{
+    private static DBConnection instance;
+
+    public DBConnection() {
+    }
+    
+    public Connection opeanConnection() throws Exception{
+        Class.forName(driverName);
+        Connection con = DriverManager.getConnection(url, user, pass);
+        return con;
+    }
+    public static DBConnection getInstance() {
+        if(instance == null){
+            instance = new DBConnection();
+        }
+        return instance;        
+    }   
+}
