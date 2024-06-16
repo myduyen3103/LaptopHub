fabc7bafc4508b05c9f67163d1fd39faccd861d0
myduyen3103 <vtmyduyen3103@gmail.com>
2024-06-16 10:38:59 +0700

    configuration dtb-infor

--- LaptopHubWeb/src/java/laptophub/config/Configuration.java
+++ LaptopHubWeb/src/java/laptophub/config/Configuration.java
@@ -11,7 +11,7 @@ package laptophub.config;
 public interface Configuration {
 
     public static String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    public static String url = "jdbc:sqlserver://127.0.0.1:1433;databaseName=J3.L.P0013;";
+    public static String url = "jdbc:sqlserver://127.0.0.1:1433;databaseName=LaptopHub;";
     public static String user = "sa";
     public static String pass = "1";
 }
